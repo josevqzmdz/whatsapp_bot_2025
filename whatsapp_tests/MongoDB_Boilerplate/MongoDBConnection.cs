6b8579a11194268a3ae1d87f30e4096ad4b3a1cd
Jose Miguel Vazquez Mendez <notanothergenericaddress@gmail.com>
2025-05-06 13:09:13 -0600

    first upload, gives ping to mongodb and thats it

---
+++ whatsapp_tests/MongoDB_Boilerplate/MongoDBConnection.cs
@@ -0,0 +1,37 @@
+﻿// boilerplate para crear una conexion entre mongoDB y ASP.NET
+// para obtener la info general del usuario de whatsapp
+
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+
+namespace whatsapp_tests.MongoDB_Boilerplate
+{
+    public class MongoDBConnection
+    {
+        private const string connectionUri = "mongodb+srv://josevqzmdz:YgtU7lc9YCBKTz8Y@primercliente.kn7tflx.mongodb.net/?retryWrites=true&w=majority&appName=primercliente";
+        private readonly MongoClient client;
+
+        public MongoDBConnection()
+        {
+            var settings = MongoClientSettings.FromConnectionString(connectionUri);
+            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+            client = new MongoClient(settings);
+        }
+
+        // simple ping para revisar conexion
+        public void Ping()
+        {
+            try
+            {
+                var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+                Console.WriteLine("Ping exitoso!");
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine($"conexion fracasada: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ whatsapp_tests/Program.cs
@@ -0,0 +1,31 @@
+using whatsapp_tests.MongoDB_Boilerplate;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+// run mongodb ping
+var mongo = new MongoDBConnection();
+mongo.Ping();
